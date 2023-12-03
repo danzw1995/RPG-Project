31c216655625e93a3067b776bc3fbcda8a6c8cf2
wang <245234166@qq.com>
2023-12-03 18:23:08 +0800

    弓箭增加非追踪类型，碰撞时校验是否死亡

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,6 +9,7 @@ namespace RPG.Combat
   public class Projectile : MonoBehaviour
   {
     [SerializeField] private float speed = 1f;
+    [SerializeField] private bool isHoming = true;
 
     private Health target;
     private float damage = 0f;
@@ -18,13 +19,17 @@ namespace RPG.Combat
     {
       if (target == null) return;
 
-      transform.LookAt(GetAimLocation());
+      if (isHoming && !target.IsDead())
+      {
+        transform.LookAt(GetAimLocation());
+      }
+
       transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-      if (other.GetComponent<Health>() == target)
+      if (other.GetComponent<Health>() == target && !target.IsDead())
       {
         target.TakeDamage(this.damage);
         Destroy(gameObject);
@@ -35,6 +40,7 @@ namespace RPG.Combat
     {
       this.target = target;
       this.damage = damage;
+      transform.LookAt(GetAimLocation());
     }
 
     private Vector3 GetAimLocation()
