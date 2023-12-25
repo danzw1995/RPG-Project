ef80e9cbff67842919adb6c9d516758fa4e71360
wang <245234166@qq.com>
2023-12-25 21:59:01 +0800

    增加quest ui

--- Assets/Asset Packs/GameDev.tv Assets/Scripts/UI/ShowHideUI.cs
+++ Assets/Asset Packs/GameDev.tv Assets/Scripts/UI/ShowHideUI.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 namespace GameDevTV.UI
 {
-    public class ShowHideUI : MonoBehaviour
+  public class ShowHideUI : MonoBehaviour
+  {
+    [SerializeField] KeyCode toggleKey = KeyCode.Escape;
+    [SerializeField] GameObject uiContainer = null;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        [SerializeField] KeyCode toggleKey = KeyCode.Escape;
-        [SerializeField] GameObject uiContainer = null;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            uiContainer.SetActive(false);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(toggleKey))
-            {
-                uiContainer.SetActive(!uiContainer.activeSelf);
-            }
-        }
+      uiContainer.SetActive(false);
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(toggleKey))
+      {
+        Toggle();
+      }
+    }
+
+    public void Toggle()
+    {
+      uiContainer.SetActive(!uiContainer.activeSelf);
+    }
+  }
 }
---
+++ Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+
+public class QuestTooltipSpawner : TooltipSpawner
+{
+  public override bool CanCreateTooltip()
+  {
+    return true;
+  }
+
+  public override void UpdateTooltip(GameObject tooltip)
+  {
+    return;
+  }
+}
