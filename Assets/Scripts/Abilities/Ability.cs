e0571b68a70138a19ef1c076e943c6420e001b2f
wang <245234166@qq.com>
2023-12-31 18:52:36 +0800

    增加 ability 商品

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Abilities/Ability", order = 0)]
+public class Ability : ActionItem
+{
+
+}
